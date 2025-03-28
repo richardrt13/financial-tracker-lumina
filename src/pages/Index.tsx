8e2b719c93c075a079f4823998f817661eeac8e1
Richard Tomé <richardrt13@gmail.com>
2025-03-28 14:44:15 -0400

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,5 +1,5 @@
 import { TransactionForm } from "@/components/TransactionForm";
-import { Dashboard } from "@/components/Dashboard";
+import { Dashboard } from "@/components/dashboard/Dashboard";
 import Header from "@/components/Header";
 
 const Index = () => {
